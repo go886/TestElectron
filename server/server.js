d76197534dc6cb972c7d77623eff002d71a37d9f
rz.li <rz.li@alibaba-inc.com>
2017-09-12 14:28:52 +0800

    net

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require("path")
 const { config } = require('./setting');
 const dsl = require('./dsl');
+const net = require('net')
 
 
 var server = express()
@@ -17,8 +18,59 @@ server.get('/js', function (req, res) {
   res.send(r.json);
 });
 
-server.listen(config.port || 8081, function (r, q) {
-  console.log('Example app listening on port 3000!');
-});
+
+
+// 检测port是否被占用
+function probe(port, callback) {
+  var server = net.createServer().listen(port)
+  var calledOnce = false
+  var timeoutRef = setTimeout(function () {
+    calledOnce = true
+    callback(false, port)
+  }, 2000)
+
+  timeoutRef.unref()
+  var connected = false
+  server.on('listening', function () {
+    clearTimeout(timeoutRef)
+
+    if (server)
+      server.close()
+
+    if (!calledOnce) {
+      calledOnce = true
+      callback(true, port)
+    }
+  })
+
+  server.on('error', function (err) {
+    clearTimeout(timeoutRef)
+
+    var result = true
+    if (err.code === 'EADDRINUSE')
+      result = false
+
+    if (!calledOnce) {
+      calledOnce = true
+      callback(result, port)
+    }
+  })
+}
+
+
+function run(port) {
+  probe(port, function (ret, port) {
+    if (ret) {
+      server.listen(port, function (r, q) {
+        console.log('Example app listening on port' + port);
+      });
+    } else {
+      run(port + 1);
+    }
+  });
+}
+run(config.port || 8081)
+
+
 server.URL = config.serverurl;
 module.exports.server = server;
